94b12536b69f9c7eaf2ad1864811f65a7d84e618
ingridlopes96 <ingrid.lopes96@gmail.com>
2023-03-06 22:08:08 -0300

    primeiro commit

---
+++ bool.dart
@@ -0,0 +1,10 @@
+void main() {
+  final a = "Ingrid";
+  final b = "DEV";
+  final isTrue = true;
+  final isFalse = false;
+
+  print(isFalse == !isTrue);
+  print(a == b);
+  print(isFalse ? "está verdadeiro" : "está falso");
+}
---
+++ variaveis.dart
@@ -0,0 +1,7 @@
+var primeiravariavel; // má prática
+var primeiraVariavel; // boa prática
+
+void main() {
+  primeiraVariavel = 1;
+  print(primeiraVariavel);
+}
